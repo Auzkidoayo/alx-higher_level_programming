a074e45c824900f8b5b49e460460fc3c3b9e592c
auzkidoayo <adedunniayokunle@gmail.com>
2024-01-19 13:36:15 -0800

    dkkd

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-"""
-Takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-"""
-
+"""Display name argument of states table"""
 import MySQLdb
-from sys import argv
-if __name__ == "__main__":
+import sys
 
-    db = MySQLdb.connect(user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         host="localhost",
-                         port=3306)
-    """Connect to a MySQL server."""
 
-    cursor = db.cursor()
+def filter_names():
+    """Takes arguments argv to list from database
+    Only lists with states that matches name argument
 
-    upper = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"\
-            .format(argv[4])
-    cursor.execute(upper)
-    lists = cusor.fetchall()
-    for row in lists:
-        if row[1] == argv[4]:
-            print(row)
+    Arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+        argv[4]: state name
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
-    cursor.close()
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
+                ORDER BY id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+
+    cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    filter_names()
